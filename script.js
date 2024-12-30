0020b531c7ca46923863fc28060f05d11e3db470
Akaash J <akaashjanarthanan@gmail.com>
2024-12-30 12:09:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let number = document.getElementById("number");
+let moistureValue = 65; // Example value for demonstration
+
+// Set the soil moisture value
+number.innerText = `${moistureValue}%`;
